a93d7aca02b557b982e4ff604460342256d1979a
ChakornK <chakornk2007@gmail.com>
2026-01-10 18:00:22 -0800

    fix: show all confessions

--- src/app/api/communities/[id]/route.js
+++ src/app/api/communities/[id]/route.js
@@ -5,20 +5,22 @@ import dbConnect from "../../db/mongodb";
 export async function GET(req, res) {
   await dbConnect();
 
-  const now = new Date()
-  const oneDayAgo = new Date(now - 24 * 60 * 60 * 1000)
+  const now = new Date();
+  const oneDayAgo = new Date(now - 24 * 60 * 60 * 1000);
 
   const community = await Community.findOne({ _id: (await res.params).id });
   const confessions = await Confession.find({
     community_id: (await res.params).id,
   });
 
-  const validConfessions = confessions.filter(confession => {
-    const secondsToLive = 10 + (confession.likes * 2)
-    const expiresAt = new Date(confession.created_at.getTime() + secondsToLive * 1000)
-    return now < expiresAt
-  })
-  return Response.json({ community, confessions: validConfessions }, { status: 200 });
+  const validConfessions = confessions.filter((confession) => {
+    const secondsToLive = 10 + confession.likes * 2;
+    const expiresAt = new Date(
+      confession.created_at.getTime() + secondsToLive * 1000,
+    );
+    return now < expiresAt;
+  });
+  return Response.json({ community, confessions }, { status: 200 });
 }
 
 export async function POST(req, res) {
